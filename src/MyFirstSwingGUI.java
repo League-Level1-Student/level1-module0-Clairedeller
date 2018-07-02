79d80331938f4bbe1674e395ef3e2be5640c5da2
league <league@worthymini-5.attlocal.net>
2018-07-01 17:05:54 -0700

    PHoto quiz and First swing

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -10,6 +10,7 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class MyFirstSwingGUI {
@@ -26,25 +27,25 @@ goot.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
 JPanel ook = new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+JLabel laq = new JLabel();
 
 
 
 
 		//7. Set the text of the JLabel to a lovely greeting.
-
+ laq.setText("hi ugly");
 		//8. Add the JPanel object to the JFrame
-
+goot.add(ook);
 		//9. Add the JLabel object to the JPanel
-
+ook.add(laq);
 		//10. Pack your JFrame.
-
+goot.pack();
 		//11. Run your program again. Do you see your message.
 		
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
+laq.setIcon(loadImage());
 		//13. Re-pack the JFrame object.
-		
+		goot.pack();
 		//14. Run the program one more time. Do you see the image?
 	}
 	
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,21 +22,23 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+String H = "http://wilshiredetroit.com/wp-content/uploads/2016/04/Steve-Buscemi-Did-It-Better-Hulk.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component Hulk;
 		// 3. use the "createImage()" method below to initialize your Component
-
+Hulk = createImage(H);
 		// 4. add the image to the quiz window
-
+quizWindow.add(Hulk);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String answer =JOptionPane.showInputDialog("Who is behind the hulk?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if (answer.equals("Steve buscemi") ) {
+	JOptionPane.showMessageDialog(null, "correct!");
+	
 		// 8. print "INCORRECT" if the answer is wrong
-
+	else (JOptionPane.showMessageDialog(null, "WRONG"));
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
@@ -49,6 +51,7 @@ public class PhotoQuiz {
 
 		// 14+ check answer, say if correct or incorrect, etc.
 
+	}
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
