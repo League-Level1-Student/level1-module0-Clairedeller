fea0a4ed16cd41c1845e3be3e666d2598634186c
Jsmith2021 <league@wts5.attlocal.net>
2018-12-02 17:13:03 -0800

    fortune cookie

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,9 +1,12 @@
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class FortuneCookie {
+public class FortuneCookie implements ActionListener {
 
 	public static void main(String[] args) {
 		FortuneCookie fc = new FortuneCookie();
@@ -15,9 +18,22 @@ public class FortuneCookie {
 		frame.setVisible(true);
 		JButton button = new JButton();
 		frame.add(button);
-		button.addActionListener((ActionListener) this);
+		button.addActionListener((this));
 
 		System.out.println("Button clicked");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, "woohoo");
+		int rand = new Random().nextInt(5);
+		if (rand==1) { JOptionPane.showMessageDialog(null, "you will fall off a cliff");}
+		if (rand==2) { JOptionPane.showMessageDialog(null, "You will eat lots of bread");}
+		if (rand==3) { JOptionPane.showMessageDialog(null, "you will explode");}
+		if (rand==4) { JOptionPane.showMessageDialog(null, "you will adopt a hamster");}
+		if (rand==5) { JOptionPane.showMessageDialog(null, "you will become vegan");}
+		// TODO Auto-generated method stub
+		
 	}
 
 }
